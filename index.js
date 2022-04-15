17b89042ed30aab9e63ba3cde27b19c6801dec39
azuiunderscore <103774633+azuiunderscore@users.noreply.github.com>
2022-04-15 12:08:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-client.login('token');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// log when the bot is ready
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// listen to messages and reply
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('hey there!');
+  }
+});
