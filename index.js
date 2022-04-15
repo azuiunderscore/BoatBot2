70c64873f9e2f849a740a1687c8ca923218fd60b
azuiunderscore <103774633+azuiunderscore@users.noreply.github.com>
2022-04-15 11:51:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in...`);
-});
-
-client.on('message', msg => {
-  msg.reply('pong');
-});
-
 client.login('token');
